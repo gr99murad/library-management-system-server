fb8636ead113cd500d170910b0af6a598f9252a5
gr99murad <golamrabbanimurad@gmail.com>
2025-01-13 21:07:23 +0600

    create api in categories

--- index.js
+++ index.js
@@ -30,6 +30,18 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+    // categories collection
+
+    const categoriesCollection = client.db('libraryManagement').collection('categories');
+
+    app.get('/categories', async(req,res) => {
+      const cursor = categoriesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
